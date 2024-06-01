fe7bcd87eefdf2f91b7447cb28559f5966f94125
leomosley <leo.mosley2@gmail.com>
2024-06-01 20:44:28 +0100

    update logout page

--- app/dashboard/logout/page.tsx
+++ app/dashboard/logout/page.tsx
@@ -4,15 +4,8 @@ import Link from "next/link";
 export default function Page() {
   return (
     <div className="flex flex-col">
-      <div className="flex gap-4">
-        <Link
-          className="flex bg-neutral-800 flex-1 px-8 py-2 rounded justify-center"
-          href="/dashboard"
-        >
-          <span className="text-neutral-200 text-lg font-bold">Cancel</span>
-        </Link>
-        <LogoutButton className="rounded" />
-      </div>
+      <span className="text-neutral-200 mb-2">Are you sure you want to logout?</span>
+      <LogoutButton className="rounded w-fit" />
     </div>
   );
 }
