3075dd24915c1eda3cb4687793c4ea2f1fb2d7f1
leomosley <leo.mosley2@gmail.com>
2024-06-07 20:25:22 +0100

    update home page styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import LinkList from "@/site/LinkList";
 import ShowList from "@/site/ShowList";
 import { Metadata } from "next";
+import Image from "next/image";
 import Link from "next/link";
 import { HiHome } from "react-icons/hi";
 
@@ -13,16 +14,25 @@ export default async function Home() {
     <main className="sm:grid sm:grid-cols-2 sm:grid-rows-2 sm:min-h-dvh">
       <section id="about" className="flex flex-col p-4 max-h-half">
         <Link
-          className="w-10 h-10 md:w-12 md:h-12 text-neutral-100"
+          className="w-12 h-12 p-2 md:w-16 md:h-16 mt-2"
           href="/"
         >
-          <HiHome className="w-full h-full" />
+          <Image 
+            className=""
+            src={"/icon.png"}
+            height={500}
+            width={500}
+            alt="icon"
+          />
         </Link>
-        <h1 className="text-2xl sm:text-4xl md:text-5xl lg:text-6xl font-extrabold tracking-tight">
+        <h1 className="text-2xl sm:text-4xl md:text-5xl lg:text-6xl font-extrabold tracking-tight mt-4">
           <span className="text-red-600">JUST&nbsp;</span>
           <span className="text-neutral-100">RADIOHEAD</span>
         </h1>
-        <span className="text-neutral-200 text-sm sm:text-base lg:text-lg mb-10">Description more desc more words and words with more words.</span>
+        <span className="text-neutral-200 text-sm sm:text-base lg:text-lg mb-10 text-balance mt-2">
+          The UK's Leading Radiohead 'Tribute' Band.
+          Respectfully performing songs from one of the 1990s and 2000s most loved bands.
+        </span>
         <div className="mt-auto max-h-[50%]">
           <LinkList />
         </div>
